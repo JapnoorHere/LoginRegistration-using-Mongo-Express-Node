b33d955319c7ba9bc12e22b64cfb858c27306e03
Japnoor Singh <109523523+JapnoorHere@users.noreply.github.com>
2024-03-13 22:19:37 +0530

    Pug module integrated for getting data from node js to html

--- back-end/server.js
+++ back-end/server.js
@@ -8,62 +8,59 @@ const port = 8000;
 const connectDB = require("./db/dbConnection");
 const User = require('./db/user');
 
-app.set('view engine','ejs');
 
 //middleware to parse json data
 app.use(express.json());
 
 // middleware to serve html as well as css file
-app.use(express.static(path.join(__dirname,"..","views")))
+app.use(express.static(path.join(__dirname, "..", "views")))
 
 //middleware to get data from html post form
-app.use(express.urlencoded({extended : true}));
-
-// Set EJS as templating engine 
+app.use(express.urlencoded({ extended: true }));
 
 
-app.get('/signup',async(req,res)=>{
-    res.render("signup");
+app.get('/signup', async (req, res) => {
+    res.render("signup.pug");
 })
 
-app.post('/signup',async(req,res)=>{
+app.post('/signup', async (req, res) => {
     // console.log(req.body);
-    try{
-        const {username,password} = req.body;
-        const user = new User({username,password});
+    try {
+        const { username, password } = req.body;
+        const user = new User({ username, password });
+        console.log(user);
         await user.save();
-        const errorFile = signupFile.replace("{{%ERROR%}}","Registration done");
-        res.send(errorFile);
+        res.render('signup.pug',{err : "Signup Successful"});
     }
-    catch{
-        const errorFile = signupFile.replace("{{%ERROR%}}","Registration failed");
-        res.send(errorFile);
+    catch (error){
+        console.log(error);
+        res.render('signup.pug',{err : "Signup Failed"});
     }
-}); 
+});
 
-app.get('/login',(req,res)=>{
-    res.render("login");
+app.get('/login', (req, res) => {
+    res.render("login.pug");
 })
 
-app.post('/login',async(req,res)=>{
-    try{    
-        const {username,password} = req.body;
-        const user = await User.findOne({username});
+app.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
         console.log(req.body);
         console.log(user);
-        if(!user){
-            res.render("login",{error : "User not found"});
+        if (!user) {
+            res.render("login.pug", { error: "User not found" });
         }
-        else if(user.password !== password){
-            console.log("Wrong password");
+        else if (user.password !== password) {
+            res.render("login.pug", { error: "Wrong password" });
+        }
+        else {
+            res.render("login.pug", { error: "Login Successful" });
 
         }
-        else{
-            console.log("Login Successful");
-        }
     }
-    catch{
-        res.status(500).json({error : "Login failed"});
+    catch {
+        res.status(500).json({ error: "Login failed" });
     }
 })
 
@@ -71,6 +68,6 @@ app.post('/login',async(req,res)=>{
 //*Database connection
 connectDB();
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("Server listening");
 })
